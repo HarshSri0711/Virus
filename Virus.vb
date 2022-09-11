ebae31509f9236a66df8f81644d41b906dc40d31
Harshit Srivastava <112399455+HarshSri0711@users.noreply.github.com>
2022-09-11 17:36:19 +0530

    Add files via upload

---
+++ Virus.vb
@@ -0,0 +1,6 @@
+Do
+msg1 = msgbox("Close this box",o,"Urgent Notice")
+msg2 = msgbox("Try again",o,"Urgent Notice")
+msg2 = msgbox("Try again",o,"Urgent Notice")
+msg3 = msgbox("Good Luck You cant Close",o,"Urgent Notice")
+Loop
